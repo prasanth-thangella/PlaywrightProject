e3a94ba57e6a9b9056976d13b191007c2d8d339b
prasanth_thangella <prasanththangella.56@gmail.com>
2026-02-28 13:41:18 +0530

    Initial commit: add .gitignore and Chapter01/01_Verfiy.js

---
+++ Chapter01/01_Verfiy.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+
+console.log(process.platform);
+// MAC - DARWIN
+// WINDOWS - WIN32
+// LINUX - LINUX
+
+console.log(process.arch);
+// x64
+// arm64
+
+
+console.log("Node Version:", process.version);
+
+
+
